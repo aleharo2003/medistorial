830e8dbb345edca842f604fbd92b20c8676fdc77
Alejandro Haro <aharo@itx.com>
2018-07-16 19:44:47 -0300

    Initial commit Create the solution Add DAL and Models projects

---
+++ Medistorial.DAL/Repos/PatientRepo.cs
@@ -0,0 +1,29 @@
+﻿using Medistorial.DAL.EF;
+using Medistorial.DAL.Repos.Base;
+using Medistorial.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Medistorial.DAL.Repos
+{
+    public class PatientRepo : RepoBase<Patient>
+    {
+        public PatientRepo(DbContextOptions<MedistorialContext> options) : base(options)
+        {
+
+        }
+        public PatientRepo()
+        {
+
+        }
+        public override IEnumerable<Patient> GetAll()
+            => Table.OrderBy(x => x.LastName);
+
+        public override IEnumerable<Patient> GetRange(int skip, int take)
+            => GetRange(Table.OrderBy(x => x.LastName), skip, take);
+
+    }
+}
